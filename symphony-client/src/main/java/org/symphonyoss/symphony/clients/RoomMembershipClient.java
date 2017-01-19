fde6e576c5e8a4e866bbfce83ca4a6f0da93468c
Kaushik Chaubal <kaushik.chaubal@blackrock.com>
2017-01-19 17:42:06 +0000

    Adding feature to add member to chat-room

--- symphony-client/src/main/java/org/symphonyoss/symphony/clients/RoomMembershipClient.java
+++ symphony-client/src/main/java/org/symphonyoss/symphony/clients/RoomMembershipClient.java
@@ -8,4 +8,18 @@ import org.symphonyoss.symphony.pod.model.MembershipList;
  */
 public interface RoomMembershipClient {
     MembershipList getRoomMembership(String id) throws SymException;
+    
+    /**
+     * Call this method to add a member to a chat room. Pass in two parameters - chat-room stream-id & user-id
+     * 
+     * @param roomStreamId
+     *            - stream-id of the chat room you want to add the member to
+     * @param userId
+     *            userId for the user in Symphony
+     * @throws Exception
+     *             throws an {@link ApiException} if there were any issues while invoking the endpoint,
+     *             {@link IllegalArgumentException} if the arguments were wrong, {@link IllegalStateException} if the
+     *             session-token is null
+     */
+    public void addMemberToRoom(String roomStreamId, long userId) throws Exception;
 }
