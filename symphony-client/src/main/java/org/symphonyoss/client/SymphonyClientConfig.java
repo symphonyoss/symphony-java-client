7e9fe0ea4db641629e6b2e5db018b39bb1cba8b4
ftbb <ftarsillo@gmail.com>
2017-06-20 18:00:00 -0400

    Updated documentation

--- symphony-client/src/main/java/org/symphonyoss/client/SymphonyClientConfig.java
+++ symphony-client/src/main/java/org/symphonyoss/client/SymphonyClientConfig.java
@@ -16,100 +16,106 @@
 
 package org.symphonyoss.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.symphonyoss.client.exceptions.ProgramFault;
+
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.symphonyoss.client.exceptions.ProgramFault;
-
+/**
+ * Configuration management
+ * <p>
+ * Will evaluate ENV and file properties
+ *
+ * @author Bruce Skingle
+ */
 public class SymphonyClientConfig {
     private static Logger log = LoggerFactory.getLogger(SymphonyClientConfig.class);
-    
-    private Properties	config = new Properties();
-    
+
+    private Properties config = new Properties();
+
     public SymphonyClientConfig() {
-	String configFile = get(SymphonyClientConfigID.SYMPHONY_CONFIG_FILE);
-	
-	if(configFile != null) {
-	    try(Reader reader = new FileReader(configFile)) {
-		config.load(reader);
-	    } catch (FileNotFoundException e) {
-		throw new ProgramFault("Config file \"" + configFile + "\" not found");
-	    } catch (IOException e) {
-		throw new ProgramFault("Config file \"" + configFile + "\" cannot be loaded", e);
-	    }
-	}
-	
-	StringBuilder s = null;
-	
-	for(SymphonyClientConfigID id : SymphonyClientConfigID.values()) {
-	    String v = get(id);
-	    if(v != null) {
-	    	log.info("%s = %s", id.getPropName(), v);
-	    }
-	    else if(id.isCore()) {
-		if(s == null) {
-		    s = new StringBuilder();
-		} else {
-		    s.append(",\n");
-		}
-		
-		s.append(id);
-	    }
-	}
-	
-	if(s != null)
-	    throw new ProgramFault("The following required properties are undefined:\n"
-		    + s.toString());
+        String configFile = get(SymphonyClientConfigID.SYMPHONY_CONFIG_FILE);
+
+        if (configFile != null) {
+            try (Reader reader = new FileReader(configFile)) {
+                config.load(reader);
+            } catch (FileNotFoundException e) {
+                throw new ProgramFault("Config file \"" + configFile + "\" not found");
+            } catch (IOException e) {
+                throw new ProgramFault("Config file \"" + configFile + "\" cannot be loaded", e);
+            }
+        }
+
+        StringBuilder s = null;
+
+        for (SymphonyClientConfigID id : SymphonyClientConfigID.values()) {
+            String v = get(id);
+            if (v != null) {
+                log.info("%s = %s", id.getPropName(), v);
+            } else if (id.isCore()) {
+                if (s == null) {
+                    s = new StringBuilder();
+                } else {
+                    s.append(",\n");
+                }
+
+                s.append(id);
+            }
+        }
+
+        if (s != null)
+            throw new ProgramFault("The following required properties are undefined:\n"
+                    + s.toString());
     }
 
     public String get(SymphonyClientConfigID id) {
-		String value = config.getProperty(id.getPropName());
-		
-		if(value == null)
-		    value = System.getProperty(id.getPropName());
-		
-		if(value == null)
-		    value = System.getenv(id.getEnvName());
-		
-		if(value == null && id.getAltName() != null)
-		    value = System.getProperty(id.getAltName());
-		
-		return value;		
-    }
-    
-    public String getRequired(SymphonyClientConfigID id) {
-		String value = get(id);
-		
-		if(value == null)
-			throw new ProgramFault("Required config parameter \"" + id + "\" is undefined.");
-		
-		return value;
-	}
-    
-    public String get(String id) {
-		String value = config.getProperty(id);
-		
-		if(value == null)
-		    value = System.getProperty(SymphonyClientConfigID.toPropName(id));
-		
-		if(value == null)
-		    value = System.getenv(SymphonyClientConfigID.toEnvName(id));
-		
-		return value;		
+        String value = config.getProperty(id.getPropName());
+
+        if (value == null)
+            value = System.getProperty(id.getPropName());
+
+        if (value == null)
+            value = System.getenv(id.getEnvName());
+
+        if (value == null && id.getAltName() != null)
+            value = System.getProperty(id.getAltName());
+
+        return value;
     }
 
-	public String getRequired(String id) {
-		String value = get(id);
-		
-		if(value == null)
-			throw new ProgramFault("Required config parameter \"" + id + "\" is undefined.");
-		
-		return value;
-	}
+    public String getRequired(SymphonyClientConfigID id) {
+        String value = get(id);
+
+        if (value == null)
+            throw new ProgramFault("Required config parameter \"" + id + "\" is undefined.");
+
+        return value;
+    }
+
+    public String get(String id) {
+        String value = config.getProperty(id);
+
+        if (value == null)
+            value = System.getProperty(SymphonyClientConfigID.toPropName(id));
+
+        if (value == null)
+            value = System.getenv(SymphonyClientConfigID.toEnvName(id));
+
+        return value;
+    }
+
+    public String getRequired(String id) {
+        String value = get(id);
+
+        if (value == null)
+            throw new ProgramFault("Required config parameter \"" + id + "\" is undefined.");
+
+        return value;
+    }
 
 }
