fbb2522fc386b7710728b9f5ae5540020c2f6719
Maurizio Pillitu <maoo@apache.org>
2018-02-05 16:54:14 +0100

    :lipstick: reverded log statements to minimise PR changes

--- symphony-client/src/main/java/org/symphonyoss/client/impl/SymphonyBasicClient.java
+++ symphony-client/src/main/java/org/symphonyoss/client/impl/SymphonyBasicClient.java
@@ -247,8 +247,6 @@ public class SymphonyBasicClient implements SymphonyClient {
     @Override
     public void init(SymAuth symAuth, SymphonyClientConfig config) throws InitException {
 
-        logger.info("Initialising Symphony Java Client...");
-
         this.config = config;
         this.symAuth = symAuth;
 
@@ -331,7 +329,6 @@ public class SymphonyBasicClient implements SymphonyClient {
         timer = new Timer("AuthRefresh:" + this.getName(), true);
         timer.scheduleAtFixedRate(authRefreshTask, SYMAUTH_REFRESH_TIME, SYMAUTH_REFRESH_TIME);
 
-        logger.info("All Symphony Java Client services resolved and configured.");
 
         //Publish MBean via JMX
         this.registerHealthMBean();
